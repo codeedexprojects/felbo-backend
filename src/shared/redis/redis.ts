206cfb843914f3e8a9853515b6e2c114ef20544a
pranaaavj <jpvelloor@gmail.com>
2026-02-16 14:36:17 +0530

    feat(auth): Removed password from redis, decided to add auth in production

--- src/shared/redis/redis.ts
+++ src/shared/redis/redis.ts
@@ -10,7 +10,6 @@ export async function connectRedis(): Promise<RedisClientType> {
       host: config.redis.host,
       port: config.redis.port,
     },
-    password: config.redis.password,
   });
 
   redisClient.on('error', (err) => {
